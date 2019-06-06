0a57a3b9c488c41c51e34a98b926c017409e8d87
hyukjung <hyukjung1013@gmail.com>
2019-06-06 22:02:41 +0900

    4th Commit: Messenger system implemented.

--- routes/index.js
+++ routes/index.js
@@ -40,21 +40,81 @@ router.post('/create_room_process', async (req, res) => {
         const newRoom = await room.save();
         const io = req.app.get('io');
         io.of('/room').emit('newRoom', newRoom);
-        res.redirect('/');
+        res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
     } catch(err) {
         console.error(err);
         next(err);
     }
-
-    res.render('main');
 });
 
 router.get('/room/:id', async (req, res, next) => {
+    try {
+        const room = await Room.findOne({ _id: req.params.id });
+        const io = req.app.get('io');
 
+        if(!room) {
+            return res.redirect('/');
+        }
+
+        if( room.password && room.password !== req.query.password ) {
+            return res.redirect('/');
+        }
+
+        const { rooms } = io.of('/chat').adapter;
+        if (rooms && rooms[req.params.id] && room.number <= rooms[req.params.id].length) {
+            return res.redirect('/');
+        }
+
+        const chats = await Chat.find({ room: room._id }).sort('createdAt');
+
+        if (chats) {
+            return res.render('room', {
+                room: room,
+                chats: chats,
+                user: req.session.color
+            });
+        } else {
+            return res.render('chat', {
+                room: room,
+                chats: [],
+                user: req.session.color
+            });
+        }
+
+    } catch(err) {
+        console.error(err);
+        return next(err);
+    }
 });
 
-router.get('/room', (req, res) => {
-    res.render('room');
+router.post('/room/:id/chat', async (req, res, next) => {
+    try {
+        const chat = new Chat({
+            room: req.params.id,
+            user: req.session.color,
+            chat: req.body.chat
+        });
+        await chat.save();
+        req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+        res.send('ok');
+    } catch(err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.delete('/room/:id', async(req, res, next) => {
+    try {
+        await Room.remove({ _id: req.params.id });
+        await Chat.remove({ room: req.params.id });
+        res.send('ok');
+        setTimeout(() => {
+            req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+        }, 2000);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
--- socket.js
+++ socket.js
@@ -1,4 +1,5 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
 
 module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, { path: '/socket.io'});
@@ -7,6 +8,10 @@ module.exports = (server, app, sessionMiddleware) => {
     // assign namespace
     const room = io.of('/room');
     const chat = io.of('/chat');
+
+    io.use((socket, next) => {
+        sessionMiddleware(socket.request, socket.request.res, next);
+    });
 
     room.on('connection', (socket) => {
         console.log(`Connected to 'room' namespace.`);
@@ -25,9 +30,32 @@ module.exports = (server, app, sessionMiddleware) => {
 
         socket.join(roomId);
 
+        socket.to(roomId).emit('join', {
+            user: 'system',
+            chat: `${req.session.color} entered.`
+        });
+
         socket.on('disconnect', () => { 
             console.log(`Disconnected to 'room' namespace.`);
             socket.leave(roomId);
+
+            const currentRoom = socket.adapter.rooms[roomId];
+            const userCount = currentRoom ? currentRoom.length : 0;
+
+            if (userCount === 0) {
+                axios.delete(`http://localhost:9004/room/${roomId}`)
+                .then(() => {
+                    console.log('Request to remove room succeed.');
+                })
+                .catch((err) => {
+                    console.log(error);
+                });
+            } else {
+                socket.to(roomId).emit('exit', {
+                    user: 'system',
+                    chat: `${req.session.color} exits.`
+                });
+            }
         });
     });
 } 
